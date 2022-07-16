d9cc6d760b45eb69f900639c9f8448aff8532d9c
Kevin Jung <kev.jung.ii@gmail.com>
2022-07-16 15:54:27 -0400

    adds page directory for authorization and directory for non-page components

---
+++ web/pages/auth/create-account.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+const createAccount: NextPage = () => {
+    return(
+        <h1>New Account Page</h1>
+    )
+}
+
+export default createAccount
---
+++ web/pages/auth/login.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+const login: NextPage = () => {
+    return(
+        <h1>Login Page</h1>
+    )
+}
+
+export default login
